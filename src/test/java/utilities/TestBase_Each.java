410a143de081e3ef453978092a5584cebefc5644
NihanZengin <nihan.zengin90@gmail.com>
2025-04-07 10:53:19 +0200

    TestBase

---
+++ src/test/java/utilities/TestBase_Each.java
@@ -0,0 +1,24 @@
+package utilities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestBase_Each {
+
+    WebDriver driver;
+    @BeforeEach
+    public void setUp(){
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+}
