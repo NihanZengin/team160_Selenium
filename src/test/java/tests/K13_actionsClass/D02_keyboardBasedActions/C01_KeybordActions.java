af7a37776fa659aef6a1432f908eaecb5561cbc6
NihanZengin <nihan.zengin90@gmail.com>
2025-04-16 10:52:17 +0200

    Actions

---
+++ src/test/java/tests/K13_actionsClass/D02_keyboardBasedActions/C01_KeybordActions.java
@@ -0,0 +1,15 @@
+package tests.K13_actionsClass.D02_keyboardBasedActions;
+
+import org.junit.jupiter.api.Test;
+import utilities.TestBase_Each;
+
+public class C01_KeybordActions extends TestBase_Each {
+
+    @Test
+    public void test01(){
+        //1- https://www.testotomasyonu.com sayfasina gidelim
+        //2- Arama kutusuna actions method’larini kullanarak “DELL Core I3” yazdirin
+        //   ve Enter’a basarak arama yaptirin
+        //3- Bulunan urun isminde “DELL Core I3” bulundugunu test edin
+    }
+}
---
+++ src/test/java/tests/K13_actionsClass/D02_keyboardBasedActions/C02_FormDoldurma.java
@@ -0,0 +1,34 @@
+package tests.K13_actionsClass.D02_keyboardBasedActions;
+
+import org.junit.jupiter.api.Test;
+import utilities.TestBase_Each;
+
+public class C02_FormDoldurma extends TestBase_Each {
+
+    @Test
+    public void test01(){
+
+        //1- https://www.testotomasyonu.com adresine gidelim
+        //2- Account linkine tiklayin
+        //3- Sign Up linkine basalim
+        //4- Ad, soyad, mail ve sifre kutularina deger yazalim
+
+        /*
+            Formdaki tum kutulari tek tek locate etmek yerine
+            firstName kutusuna click yapip, isim gonderelim
+            sonraki kutulara gecisi de TAB tusu ile yapabiliriz
+         */
+
+        //   ve Sign Up butonuna basalim
+
+        //5- Kaydin olusturuldugunu test edin
+
+        /*
+            Kayit yapinca bizi otomatik olarak giris sayfasina yonlendiriyor
+            kaydin olustugunu test edebilmek icin
+            giris yapalim
+         */
+
+
+    }
+}
